3328d62fd9ba34d9b7dd16183601f56e244ad73d
Mantosh Bhawal <mantoshbhawal@gmail.com>
2023-04-07 14:43:17 +0530

    intial comment

---
+++ features.js
@@ -0,0 +1,9 @@
+// const gfname = 'jyoti';
+// const gfname1 = 'jyoti1';
+// const gfname2 = 'jyoti2';
+
+// export default gfname;
+// export { gfname1, gfname2 };
+export const random = () => {
+  return `${~~(Math.random() * 100)}%`;
+};
